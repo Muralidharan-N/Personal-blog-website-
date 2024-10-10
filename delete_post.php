d921e8695bff0e367e9f6d98096a7b1d9f08b695
Muralidharan N <117349353+Muralidharan-N@users.noreply.github.com>
2024-10-10 11:09:11 +0530

    Create delete_post.php

---
+++ delete_post.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require 'db.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+}
+
+$post_id = $_GET['id'];
+$stmt = $pdo->prepare("DELETE FROM posts WHERE id = ? AND
