60e0b21a258b9865f6216487867daa7c140c1fb0
Muralidharan N <117349353+Muralidharan-N@users.noreply.github.com>
2024-10-10 11:07:44 +0530

    Create manage_post.php

---
+++ manage_post.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require 'db.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+}
+
+$user_id = $_SESSION['user_id'];
+$posts = $pdo->prepare("SELECT * FROM posts WHERE user_id = ?");
+$posts->execute([$user_id]);
+?>
+
+<div class="container">
+    <h2>Your Posts</h2>
+    <a href="add_post.php">Add New Post</a>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Created Date</th>
+            <th>Actions</th>
+        </tr>
+        <?php while ($post = $posts->fetch()): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($post['title']); ?></td>
+            <td><?php echo htmlspecialchars($post['status']); ?></td>
+            <td><?php echo $post['created_at']; ?></td>
+            <td>
+                <a href="edit_post.php?id=<?php echo $post['id']; ?>">Edit</a>
+                <a href="delete_post.php?id=<?php echo $post['id']; ?>">Delete</a>
+            </td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+</div>
